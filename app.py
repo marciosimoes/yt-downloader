6913eb12c11764e20b99aad0fa0b40f8d4b4fca6
Marcio Simões <eu@marciosimoes.com>
2022-12-28 00:51:44 -0300

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, url_for, redirect, send_file, session
+from pytube import YouTube
+from io import BytesIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "3d6f45a5fc12445dbac2f59c3b6c7cb1"
+
+@app.route("/", methods = ["GET", "POST"])
+def home():
+    if request.method == "POST":
+        session['link'] = request.form.get('url')
+        try:
+            url = YouTube(session['link'])
+            url.check_availability()
+        except:
+            return render_template("error.html")
+        return render_template("download.html", url=url)
+    return render_template("home.html")
+
+@app.route("/download", methods = ["GET", "POST"])
+def download_video():
+    if request.method == "POST":
+        buffer = BytesIO()
+        url = YouTube(session['link'])
+        itag = request.form.get("itag")
+
+        video = url.streams.get_by_itag(itag)
+        video.stream_to_buffer(buffer)
+
+        buffer.seek(0)
+
+        return send_file(buffer, 
+            as_attachment=True, 
+            attachment_filename=url.title + '.mp4',
+            mimetype="video/mp4")
+
+    return redirect(url_for("home"))
+
+if __name__ == '__main__':
+    app.run()
